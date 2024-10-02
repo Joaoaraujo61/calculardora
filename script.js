3cb9433e3a6ea2388fa202931f30dd47503d6494
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2024-10-02 12:00:32 -0300

    print number in result area

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+
 const del= document.getElementById('delete')
 const res = document.querySelector('div#resultado')
+const [zero, one, two, three, four, five, six, seven, eight, nine] = document.querySelectorAll('#zero, #um, #dois, #tres, #quatro, #cinco, #seis, #sete, #oito, #nove')
+const num = [zero, one, two, three, four, five, six, seven, eight, nine]
+var dbNum = []
 
-res.innerHTML = 'iii'
+num.forEach(button =>{
+    button.addEventListener('click', print)
+})
+
+function print(){
+    dbNum.push(this.value)
+    let output = ''
+    for(let pos in dbNum){
+        output += dbNum[pos]
+        res.innerHTML = output
+    }
+    Number(output)
+}
