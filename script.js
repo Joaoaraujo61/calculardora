d706fbaf3140e581fe64e65554330d09ac4b10ee
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2024-10-05 01:59:19 -0300

    apagar function added

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-
-const del= document.getElementById('delete')
 const res = document.querySelector('div#resultado')
 const [zero, one, two, three, four, five, six, seven, eight, nine] = document.querySelectorAll('#zero, #um, #dois, #tres, #quatro, #cinco, #seis, #sete, #oito, #nove')
 const num = [zero, one, two, three, four, five, six, seven, eight, nine]
 var dbNum = []
 
+const backSpace = document.getElementById('delete')
+
 num.forEach(button =>{
     button.addEventListener('click', print)
 })
 
+backSpace.addEventListener('click', apagar)
+var output = 0
+
 function print(){
     dbNum.push(this.value)
-    let output = ''
-    for(let pos in dbNum){
-        output += dbNum[pos]
-        res.innerHTML = output
-    }
-    Number(output)
+    output = dbNum.join('')
+    res.innerHTML = output
+}
+function apagar(){
+    dbNum.pop()
+    output = dbNum.join('')
+    res.innerHTML = output
 }
