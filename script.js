7478b45eaaca288c21cbec0f349c54fd806d4660
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2024-10-07 12:19:28 -0300

    printCarac added, print operator

--- script.js
+++ script.js
@@ -4,14 +4,33 @@ const num = [zero, one, two, three, four, five, six, seven, eight, nine]
 var dbNum = []
 
 const backSpace = document.getElementById('delete')
+const sum = document.getElementById('adicao')
+const sub = document.getElementById('sub')
+const div = document.getElementById('divisao')
+const mult = document.getElementById('mult')
+const percent = document.getElementById('porcent')
+const poten = document.getElementById('potencia')
+const root = document.getElementById('raiz')
+const equal = document.getElementById('equal')
+
+sum.addEventListener('click', printCarac, soma)
+sub.addEventListener('click',printCarac, subtr)
+div.addEventListener('click',printCarac, divis)
+mult.addEventListener('click',printCarac, multip)
+percent.addEventListener('click',printCarac, porcen)
+poten.addEventListener('click',printCarac, potencial)
+root.addEventListener( 'click',printCarac, raiz)
+
+var oper = [sum.value, sub.value, div.value, mult.value, percent.value, poten.value, root.value]
 
 num.forEach(button =>{
     button.addEventListener('click', print)
 })
 
 backSpace.addEventListener('click', apagar)
-var output = 0
 
+
+var output 
 function print(){
     dbNum.push(this.value)
     output = dbNum.join('')
@@ -21,4 +40,47 @@ function apagar(){
     dbNum.pop()
     output = dbNum.join('')
     res.innerHTML = output
+}
+
+function printCarac(){
+    let lastChar = dbNum[dbNum.length - 1]
+    if(lastChar == this.value){
+        return
+    }else if(dbNum.length == 0){
+        return
+    }else if(oper.includes(lastChar)){
+        return
+    }else{
+        dbNum.push(this.value)
+        output = dbNum.join('')
+        res.innerHTML = output
+    }
+}
+
+function soma(){
+      
+}
+
+function subtr(){
+
+}
+
+function divis(){
+
+}
+
+function multip(){
+
+}
+
+function porcen(){
+
+}
+
+function potencial(){
+
+}
+
+function raiz(){
+
 }
